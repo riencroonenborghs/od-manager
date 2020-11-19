ae1880e6571fc93b8aa08d5f01b87b6ba4bd87ba
Rien Croonenborghs <rien@croonenborghs.net>
2020-11-19 17:45:19 +1300

    background

--- src/background.js
+++ src/background.js
@@ -1,3 +1,21 @@
-browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
-  console.log('Hello from the background')
-})
+import Vue from 'vue'
+import VueResource from 'vue-resource'
+import { BackgroundDownloadsService } from '@/services/background_downloads_service'
+
+Vue.use(VueResource)
+const service = new BackgroundDownloadsService(Vue.http)
+
+// browser.runtime.onMessage.addListener(function (request, sender, sendResponse) {
+//   console.log('Hello from the background')
+// })
+
+let time = 0
+setInterval(function () {
+  chrome.browserAction.setBadgeText({ text: time.toString() })
+  ++time
+  service.all().then(
+    (downloads) => {
+      console.log(downloads)
+    }
+  )
+}, 1000 * 5)
